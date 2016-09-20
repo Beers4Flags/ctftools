ef4f486abd256faabc886e0415b6d89c8949dffb
David Rossier <david.rossier@nagra.com>
2016-09-20 11:15:59 +0200

    [*] make it generic

--- http/http_cookie.py
+++ http/http_cookie.py
@@ -30,39 +30,9 @@ def dump():
     for cookie in cookies:
         print cookie.name, cookie.value
 
-res = fetch("http://challenge01.root-me.org/programmation/ch7/")
+res = fetch("uri")
 res = res.read()
 
-p = re.compile('.*base64,(.*)" /><br/>.*')
-d=p.sub(r'\1', res)
-f = open("qr.png", 'w')
-f.write(base64.b64decode(d))
-f.close()
-#image saved.
-
-from PIL import Image
-import zbarlight
-imflag = Image.open("flag.png")
-flag = imflag.load()
-im = Image.open("qr.png")
-pix = im.load()
-height,width = im.size
-
-for i in range(0,height):
-  for j in range(0,width):
-    s1,s2,s3 = flag[i,j]
-    #s2 = flag[i,j]
-    if s2 <= 150:
-      pix[i,j] = 0,0,0
-
-im.save("qrblack.png")
-
-
-img = Image.open("qrblack.png").transpose(Image.FLIP_LEFT_RIGHT)
-
-res = zbarlight.scan_codes('qrcode', img)
-print res[0][11:]
-
-params = urllib.urlencode({'metu': res[0][11:], 'submit': 'Try'})
-response = post("http://challenge01.root-me.org/programmation/ch7/", params)
+params = urllib.urlencode({'metu': value, 'submit': 'Try'})
+response = post("uri", params)
 print response.read()
